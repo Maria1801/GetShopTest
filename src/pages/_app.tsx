fdf00f91453b8ea9bf4fdc14d9676849cd1cb7f8
Maria Mazur <mmazur18@yandex.ru>
2024-10-12 23:51:45 +0300

    почистила ненужные импорты

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import Layout from "@/components/shared/Layout";
 import { ActiveSectionProvider } from "@/providers/ActiveSectionContext";
 import { ThemeUIProvider } from 'theme-ui';
-import '../static/jank-empty.css';
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
